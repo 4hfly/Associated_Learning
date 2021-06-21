4714bb3b6cf8e41292b1fa070c703ba3fd0c9eac
林諦南 <Lindinan934301@gmail.com>
2021-06-22 00:12:54 +0800

    Linear_AL done.

--- model.py
+++ model.py
@@ -1,46 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import torch
 import torch.nn as nn
 
 from torch import Tensor
+from torch.nn.modules.module import Module
+
+
+CONFIG = {
+    "loss_function": nn.MSELoss(),
+    "hidden_size": 128,
+    "num_layers": 1,
+    "batch_size": 1,
+    "dropout": 0,
+    "bidirectional": False,
+}
+
+MODELS = {
+    "Linear": nn.Linear,
+    "LSTM": nn.LSTM,
+    "GRU": nn.GRU,
+}
 
 
 class AutoEncoder(nn.Module):
-    r"""
 
-    """
-
-    #    _t   : g(y) = encoded y
+    #       _t: g(y) = encoded y
     # _t_prime: h(g(y)) = ae output
     y: Tensor
     _t: Tensor
     _t_prime: Tensor
 
-    # NOTE: 論文上的版本應該是 linear，不過我不曉得用其他類型的 AE 會有什麼差異，
+    # NOTE: 我不曉得用其他類型的 AE 會有什麼差異，
     # 也許可以嘗試其他類型的，所以這部分就標記成NOTE。
-    def __init__(self, input_size, hidden_size=2) -> None:
-        """
-        TODO: 看起來只有一層，loss func. 是 MSE，optimizer 是 Adam，放著備忘用。
+    def __init__(
+        self,
+        g: Module,
+        input_size: int,
+        hidden_size: int
+    ) -> None:
+        r"""
 
         Args:
-            input_size: the total pixels of an image
+            mode: for g function
+            input_size: y_size
             hidden_size:
         """
         super(AutoEncoder, self).__init__()
 
         # g function
-        self.encoder = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.Sigmoid()
-        )
+        self.encoder = g
         # h function
         self.decoder = nn.Sequential(
             nn.Linear(hidden_size, input_size),
             nn.Sigmoid()
         )
-        self.criterion = nn.MSELoss()
+        self.criterion = CONFIG["loss_function"]
 
     def forward(self, y):
 
@@ -63,22 +78,52 @@ class ALComponent(nn.Module):
     _s_prime: Tensor
     _t_prime: Tensor
 
-    def __init__(self, x_size, y_size, hidden_size=2) -> None:
+    def __init__(
+        self,
+        mode: str,
+        input_size: int,
+        output_size: int,
+        hidden_size: int,
+        num_layers: int = CONFIG["num_layers"],
+        batch_size: int = CONFIG["batch_size"],
+        dropout: float = CONFIG["dropout"],
+        bidirectional: bool = CONFIG["bidirectional"],
+    ) -> None:
 
         super(ALComponent, self).__init__()
-        # ELU:
-        # https://pytorch.org/docs/stable/generated/torch.nn.ELU.html#torch.nn.ELU
-        self.f = nn.ELU()
+        # f function
+        # TODO: 這麼寫主要是為了擴充性，假如各種網路拆開來寫不使用繼承，
+        # 那麼 f_function 就不需要這麼多 if else。
+        if mode == "Linear":
+            self.f = MODELS[mode](input_size, input_size)
+            self.g = MODELS[mode](output_size, hidden_size)
+
+        # NOTE: Pytorch 自己會把 dropout 轉成 float。
+        elif mode == "LSTM" or mode == "GRU":
+            self.f = MODELS[mode](
+                input_size,
+                hidden_size,
+                num_layers,
+                dropout=dropout,
+                bidirectional=bidirectional
+            )
+            self.g = MODELS[mode](
+                output_size,
+                hidden_size,
+                num_layers,
+                dropout=dropout,
+                bidirectional=bidirectional
+            )
 
         # bridge function
-        # TODO: 不確定這個 bridge 該如何設計，看原版用 sigmoid，我先用 linear 確保 size
-        # 可調整。
+        # TODO: 不確定這個 bridge 該如何設計。
         self.b = nn.Sequential(
-            nn.Linear(x_size, hidden_size),
+            nn.Linear(input_size, hidden_size),
             nn.Sigmoid()
         )
-        self.ae = AutoEncoder(y_size)
-        self.criterion = nn.MSELoss()
+        self.batch_size = batch_size
+        self.ae = AutoEncoder(self.g, output_size, hidden_size)
+        self.criterion = CONFIG["loss_function"]
 
     def forward(self, x, y):
 
@@ -93,94 +138,73 @@ class ALComponent(nn.Module):
         return self.criterion(self._s_prime, self._t) + self.ae.loss()
 
 
-class Model(nn.Module):
+class Linear_AL(ALComponent):
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.x = torch.ones(8)
-        self.y = torch.zeros(8)
-        self.model = ALComponent(8, 8)
+    def __init__(self, *args, **kwargs) -> None:
 
-    def forward(self):
-
-        x_out, y_out = self.model(self.x, self.y)
-        return x_out, y_out
-
-    def loss(self):
-
-        return self.model.loss()
+        super(Linear_AL, self).__init__("Linear", *args, **kwargs)
 
 
-class ResNet(nn.Module):
+class LSTM_AL(ALComponent):
+
+    def __init__(self, *args, **kwargs) -> None:
+
+        super(LSTM_AL, self).__init__("LSTM", *args, **kwargs)
+
+    def forward(self, input, hx=None):
+
+        return
+
+
+class ResNet_AL(ALComponent):
     """https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py"""
 
-    def __init__(self) -> None:
-        """"""
-        super(ResNet, self).__init__()
+    def __init__(self, *args, **kwargs) -> None:
+
+        super(ResNet_AL, self).__init__("ResNet_AL", *args, **kwargs)
 
 
-class VGG(nn.Module):
+class VGG_AL(ALComponent):
     """https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py"""
 
-    def __init__(self) -> None:
-        """"""
-        super(VGG, self).__init__()
+    def __init__(self, *args, **kwargs) -> None:
+
+        super(VGG_AL, self).__init__("VGG_AL", *args, **kwargs)
 
 
-class CNN(nn.Module):
+class CNN_AL(nn.Module):
 
-    def __init__(self, input_size, output_size) -> None:
-        """"""
-        super(CNN, self).__init__()
-        self.kernel_size = 3
-        self.stride = 2
-        self.layers = nn.Sequential(
-            nn.conv2d(input_size, output_size, self.kernel_size, self.stride),
-            nn.ReLU(),
-            nn.MaxPool2d(),
-        )
+    def __init__(self, *args, **kwargs) -> None:
 
-    def forward(self, x):
-
-        return self.layers(x)
+        super(CNN_AL, self).__init__("CNN_AL", *args, **kwargs)
 
 
-class MLP(nn.Module):
-    """"""
+class MLP_AL(nn.Module):
 
-    # TODO: 還有更改空間，原版似乎複雜一點。
-    def __init__(self, input_size, output_size, hidden_size=1024) -> None:
-        """"""
-        super(MLP, self).__init__()
-        self.flatten = nn.Flatten()
-        self.layers = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size),
-        )
+    def __init__(self, *args, **kwargs) -> None:
 
-    def forward(self, x):
-
-        x = self.flatten(x)
-        return self.layers(x)
+        super(MLP_AL, self).__init__("MLP_AL", *args, **kwargs)
 
 
 def test():
 
-    model = Model()
+    inputs = torch.randn(8)
+    outputs = torch.randn(8)
+    model = Linear_AL(len(inputs), len(outputs), 2)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    for _ in range(3):
+    epochs = 5
+    for _ in range(epochs):
 
-        x_out, y_out = model()
+        x_out, y_out = model(inputs, outputs)
         loss = model.loss()
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        print(inputs)
+        print(outputs)
         print(x_out, y_out)
         print(loss.item())
 
