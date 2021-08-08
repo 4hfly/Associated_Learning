76bc2a8a2fd52f115459d944d8837f039df01168
林諦南 <Lindinan934301@gmail.com>
2021-08-08 20:55:06 +0800

    Add classification.

---
+++ classification/model.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+from typing import List, Optional, Tuple
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torchtext.vocab import Vectors
+
+CONFIG = {
+    "loss_function": "MSE",
+    "decoder": "attn",
+    "hidden_size": 128,
+    "num_layers": 1,
+    "bias": True,
+    "batch_first": False,
+    "dropout": 0.,
+    "bidirectional": True,
+    "vocab_size": (25000, 25000),
+    "embedding_dim": (300, 128)
+}
+
+
+class ALComponent(nn.Module):
+
+    x: Tensor
+    y: Tensor
+    _s: Tensor
+    _t: Tensor
+    _s_prime: Tensor
+    _t_prime: Tensor
+
+    def __init__(
+        self,
+        f: nn.Module,
+        g: nn.Module,
+        bx: nn.Module,
+        by: nn.Module,
+        dx: nn.Module,
+        dy: nn.Module,
+        cb: nn.Module,
+        ca: nn.Module,
+        reverse: bool = False
+    ) -> None:
+
+        super(ALComponent, self).__init__()
+
+        self.f = f
+        self.g = g
+        # birdge function
+        self.bx = bx
+        self.by = by
+        # decoder h function
+        self.dx = dx
+        self.dy = dy
+        # loss function for bridge and auto-encoder
+        self.criterion_br = cb
+        self.criterion_ae = ca
+
+        self.dropout = nn.Dropout(CONFIG["dropout"])
+        self.reverse = reverse
+
+    def forward(self, x, y):
+
+        self.x = x
+        self.y = y
+
+        if self.training:
+
+            self._s = self.f(x)
+            self._s_prime = self.dx(self._s)
+            self._t = self.g(y)
+            self._t_prime = self.dy(self._t)
+            return self._s.detach(), self._t.detach()
+
+        else:
+
+            if not self.reverse:
+                self._s = self.f(x)
+                self._t_prime = self.dy(self.bx(self._s))
+                return self._s.detach(), self._t_prime.detach()
+            else:
+                self._t = self.g(x)
+                self._s_prime = self.dx(self.by(self._t))
+                return self._t.detach(), self._s_prime.detach()
+
+    def loss(self):
+
+        if not self.reverse:
+            loss_b = self.criterion_br(self.bx(self._s), self._t)
+            loss_d = self.criterion_ae(self._t_prime, self.y)
+        else:
+            loss_b = self.criterion_br(self.by(self._t), self._s)
+            loss_d = self.criterion_ae(self._s_prime, self.x)
+
+        return loss_b + loss_d
+
+
+class EmbeddingAL(ALComponent):
+    """
+    For classification.
+    """
+
+    def __init__(
+        self,
+        num_embeddings: Tuple[int, int],
+        embedding_dim: Tuple[int, int],
+        pretrained: Vectors = None,
+        padding_idx: int = 0,
+        reverse: bool = False
+    ) -> None:
+
+        if pretrained:
+            embeddings = torch.FloatTensor(pretrained.vectors)
+            f = nn.Embedding.from_pretrained(
+                embeddings, padding_idx=padding_idx)
+        else:
+            f = nn.Embedding(
+                num_embeddings[0], embedding_dim[0], padding_idx=padding_idx)
+        g = nn.Embedding(
+            num_embeddings[1], embedding_dim[1], padding_idx=padding_idx)
+        # bridge function
+        bx = nn.Linear(embedding_dim[0], embedding_dim[1])
+        by = nn.Linear(embedding_dim[1], embedding_dim[0])
+        # h function
+        dx = nn.Linear(embedding_dim[0], num_embeddings[0])
+        dy = nn.Linear(embedding_dim[1], num_embeddings[1])
+        # loss function
+        cb = nn.MSELoss()
+        ca = nn.BCEWithLogitsLoss()
+
+        super(EmbeddingAL, self).__init__(
+            f, g, bx, by, dx, dy, cb, ca, reverse=reverse)
+
+
+class LinearAL(ALComponent):
+    """
+    For classification.
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        hidden_size: Tuple[int, int],
+        bias: bool = True,
+        reverse: bool = False
+    ) -> None:
+
+        f = nn.Linear(in_features, hidden_size[0], bias=bias)
+        g = nn.Linear(out_features, hidden_size[1], bias=bias)
+        # bridge function
+        bx = nn.Linear(hidden_size[0], hidden_size[1])
+        by = nn.Linear(hidden_size[1], hidden_size[0])
+        # h function
+        dx = nn.Linear(hidden_size[0], in_features)
+        dy = nn.Linear(hidden_size[1], out_features)
+        # loss function
+        cb = nn.MSELoss()
+        ca = nn.MSELoss()
+
+        super(LinearAL, self).__init__(
+            f, g, bx, by, dx, dy, cb, ca, reverse=reverse)
+
+
+class LSTMAL(ALComponent):
+    """
+    For classification.
+    """
+
+    _h_nx: Tensor
+    _h_ny: Tensor
+
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        hidden_size: Tuple[int, int],
+        num_layers: int = 1,
+        bias: bool = True,
+        batch_first: bool = False,
+        dropout: float = 0.,
+        bidirectional: bool = False,
+        reverse: bool = False
+    ) -> None:
+
+        f = nn.LSTM(
+            input_size,
+            hidden_size[0],
+            num_layers,
+            bias=bias,
+            batch_first=batch_first,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+        g = nn.Linear(output_size, hidden_size[1])
+        # bridge function
+        bx = nn.Linear(hidden_size[0], hidden_size[1])
+        by = nn.Linear(hidden_size[1], hidden_size[0])
+        # h function
+        dx = nn.LSTM(hidden_size[0], input_size)
+        dy = nn.Linear(hidden_size[1], output_size)
+        # loss function
+        cb = nn.MSELoss()
+        ca = nn.MSELoss()
+
+        super(LSTMAL, self).__init__(
+            f, g, bx, by, dx, dy, cb, ca, reverse=reverse)
+
+    def forward(
+        self,
+        x: Tensor,
+        y: Tensor,
+        hx: Optional[Tuple[Tensor, Tensor]] = None,
+        hy: Optional[Tuple[Tensor, Tensor]] = None
+    ):
+        """https://github.com/pytorch/pytorch/blob/700df82881786f3560826f194aa9e04beeaa3fd8/torch/nn/modules/rnn.py#L659\n
+        Args:\n
+            x: (L, N, Hin) or (N, L, Hin)\n
+            y: (L, N, Hout) or (N, L, Hout)\n
+            hx: ((D * num_layers, N, Hout), (D * num_layers, N, Hcell))\n
+            hy: ((D * num_layers, N, Hout), (D * num_layers, N, Hcell))\n
+            L = sequence length\n
+            N = batch size\n
+            D = bidirectional\n
+        Returns:\n
+            x outputs: output x, (hx_n, cx_n)\n
+            y outputs: output y, (hy_n, hy_n)\n
+        """
+
+        self.x = x
+        self.y = y
+
+        if self.training:
+
+            self._s, (self._h_nx, c_nx) = self.f(x, hx)
+            self._s_prime = self.dx(self._s)
+            self._t = self.g(y, hy)
+            self._t_prime = self.dy(self._t)
+            return self._s.detach(), (self._h_nx, c_nx), self._t.detach()
+
+        else:
+
+            if not self.reverse:
+                self._s, (self._h_nx, c_nx) = self.f(x, hx)
+                self._t_prime = self.dy(self.bx(self._s))
+                return self._s.detach(), (self._h_nx, c_nx), self._t_prime.detach()
+            else:
+                raise Exception()
+
+    def loss(self):
+
+        if not self.reverse:
+            loss_b = self.criterion_br(self.bx(self._s), self._t)
+            loss_d = self.criterion_ae(self._t_prime, self.y)
+        else:
+            raise Exception()
+
+        return loss_b + loss_d
---
+++ classification/trainer.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from collections import Counter
+
+import time
+import torch
+import torch.nn as nn
+import tqdm
+from torch.utils.data import DataLoader
+from torchtext.data.functional import to_map_style_dataset
+from torchtext.data.utils import get_tokenizer
+from torchtext.datasets import IMDB
+from torchtext.vocab import Vocab
+
+from model import LSTMAL, EmbeddingAL
+
+
+class Dataset(object):
+
+    def __init__(self, train_data) -> None:
+
+        self.create_vocab()
+        self.vocab_size = self.vocab.__len__
+        self.dataloader = DataLoader(
+            train_data, batch_size=8, collate_fn=self.collate_fn)
+
+    def create_vocab(self):
+
+        tokenizer = get_tokenizer('basic_english')
+        counter = Counter()
+        with tqdm(unit_scale=0, unit='lines') as t:
+            for tokens in self.yield_tokens(self.train_data, tokenizer):
+                counter.update(tokens)
+                t.update(1)
+
+        self.vocab = Vocab(counter, vectors='fasttext.en.300d')
+        self.text_pipeline = lambda x: self.vocab(tokenizer(x))
+        self.label_pipeline = lambda x: 1 if x == 'pos' else 0
+
+    @staticmethod
+    def yield_tokens(iter, tokenizer):
+
+        for _, text in iter:
+            yield tokenizer(text)
+
+    def collate_fn(self, batch):
+
+        label_list, text_list, offsets = [], [], [0]
+
+        for (_label, _text) in batch:
+            label_list.append(self.label_pipeline(_label))
+            processed_text = torch.tensor(
+                self.text_pipeline(_text), dtype=torch.int64)
+            text_list.append(processed_text)
+            offsets.append(processed_text.size(0))
+
+        label_list = torch.tensor(label_list, dtype=torch.int64)
+        offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+        text_list = torch.cat(text_list)
+
+        return label_list.to(self.device), text_list.to(self.device), offsets.to(self.device)
+
+
+class Trainer(object):
+
+    def __init__(self, dataset) -> None:
+
+        self.dataloader = dataset.dataloader
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+
+        # TODO: emb_size for y
+        # TODO: magic number (300, 2)
+        self.embedding = EmbeddingAL((dataset.vocab_size, 2), (300))
+        self.layer_1 = LSTMAL()
+        self.layer_2 = LSTMAL()
+        self.model = nn.Sequential(
+            self.embedding,
+            self.layer_1,
+            self.layer_2
+        )
+        self.optimizer_1 = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+        self.optimizer_2 = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+        self.optimizer_3 = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+
+    def train(self, epoch):
+
+        self.model.train()
+
+        # log params
+        total_acc, total_count = 0, 0
+        log_interval = 500
+        start_time = time.time()
+
+        for idx, (label, text) in enumerate(self.dataloader):
+
+            self.optimizer_1.zero_grad()
+            self.optimizer_2.zero_grad()
+            self.optimizer_3.zero_grad()
+
+            # emb layer
+            x, y = self.embedding(text, label)
+            loss = self.embedding.loss()
+            loss.backward()
+            self.optimizer_1.step()
+
+            # 1st lstm
+            x, hx, y = self.layer_1(x, y)
+            loss = self.layer_1.loss()
+            loss.backward()
+            self.optimizer_2.step()
+
+            # 2nd lstm
+            x, hx, y = self.layer_2(x, y, hx)
+            loss = self.layer_2.loss()
+            loss.backward()
+            self.optimizer_3.step()
+
+            # inference
+            left = self.layer_2.f(self.layer_1.f(self.embedding(text)))
+            right = self.layer_2.bx(left)
+            predicted_label = self.embedding.dy(
+                self.layer_1.dy(self.layer_2.dy(right)))
+
+            total_acc += (predicted_label.argmax(1) == label).sum().item()
+            total_count += label.size(0)
+            if idx % log_interval == 0 and idx > 0:
+                elapsed = time.time() - start_time
+                print(
+                    f'| epoch {epoch:3d} | {idx:5d}/{len(self.dataloader):5d} batches | accuracy {total_acc/total_count:8.3f}')
+                total_acc, total_count = 0, 0
+                start_time = time.time()
+
+    def evaluate(self):
+
+        self.model.eval()
+
+        total_acc, total_count = 0, 0
+
+        with torch.no_grad():
+            for label, text in self.dataloader:
+                left = self.layer_2.f(self.layer_1.f(self.embedding(text)))
+                right = self.layer_2.bx(left)
+                predicted_label = self.embedding.dy(
+                    self.layer_1.dy(self.layer_2.dy(right)))
+
+            total_acc += (predicted_label.argmax(1) == label).sum().item()
+            total_count += label.size(0)
+
+        return total_acc/total_count
+
+
+def main():
+
+    train_iter, test_iter = IMDB()
+    train_data = to_map_style_dataset(train_iter)
+    test_data = to_map_style_dataset(test_iter)
+    dataset = Dataset(train_data)
+    trainer = Trainer(dataset)
+
+    for epoch in range(1, 11):
+        epoch_start_time = time.time()
+        trainer.train(epoch)
+
+
+if __name__ == '__main__':
+    main()
